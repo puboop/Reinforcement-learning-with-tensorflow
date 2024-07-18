88e30f1959cba37c0b6a12dd519a2c3bd42b11b4
desk-white <desk-white@163.com>
2024-07-18 10:04:43 +0800

    ddpg

--- contents/9_Deep_Deterministic_Policy_Gradient_DDPG/DDPG_update2.py
+++ contents/9_Deep_Deterministic_Policy_Gradient_DDPG/DDPG_update2.py
@@ -19,15 +19,14 @@ import numpy as np
 import gym
 import time
 
-
 #####################  hyper parameters  ####################
 
 MAX_EPISODES = 200
 MAX_EP_STEPS = 200
-LR_A = 0.001    # learning rate for actor
-LR_C = 0.002    # learning rate for critic
-GAMMA = 0.9     # reward discount
-TAU = 0.01      # soft replacement
+LR_A = 0.001  # learning rate for actor
+LR_C = 0.002  # learning rate for critic
+GAMMA = 0.9  # reward discount
+TAU = 0.01  # soft replacement
 MEMORY_CAPACITY = 10000
 BATCH_SIZE = 32
 
@@ -39,7 +38,7 @@ ENV_NAME = 'Pendulum-v0'
 
 
 class DDPG(object):
-    def __init__(self, a_dim, s_dim, a_bound,):
+    def __init__(self, a_dim, s_dim, a_bound, ):
         self.memory = np.zeros((MEMORY_CAPACITY, s_dim * 2 + a_dim + 1), dtype=np.float32)
         self.pointer = 0
         self.sess = tf.Session()
@@ -49,24 +48,32 @@ class DDPG(object):
         self.S_ = tf.placeholder(tf.float32, [None, s_dim], 's_')
         self.R = tf.placeholder(tf.float32, [None, 1], 'r')
 
-        self.a = self._build_a(self.S,)
+        self.a = self._build_a(self.S, )
         q = self._build_c(self.S, self.a, )
+        # 获取 Actor 和 Critic 网络的可训练参数
         a_params = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='Actor')
         c_params = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='Critic')
-        ema = tf.train.ExponentialMovingAverage(decay=1 - TAU)          # soft replacement
+        # 定义目标网络的参数替换策略（软更新）
+        ema = tf.train.ExponentialMovingAverage(decay=1 - TAU)  # soft replacement
 
         def ema_getter(getter, name, *args, **kwargs):
             return ema.average(getter(name, *args, **kwargs))
 
-        target_update = [ema.apply(a_params), ema.apply(c_params)]      # soft update operation
-        a_ = self._build_a(self.S_, reuse=True, custom_getter=ema_getter)   # replaced target parameters
+        # 定义目标网络更新操作
+        target_update = [ema.apply(a_params), ema.apply(c_params)]  # soft update operation
+        a_ = self._build_a(self.S_, reuse=True, custom_getter=ema_getter)  # replaced target parameters
         q_ = self._build_c(self.S_, a_, reuse=True, custom_getter=ema_getter)
-
+        # 定义 Actor 网络的损失和训练操作
         a_loss = - tf.reduce_mean(q)  # maximize the q
         self.atrain = tf.train.AdamOptimizer(LR_A).minimize(a_loss, var_list=a_params)
-
-        with tf.control_dependencies(target_update):    # soft replacement happened at here
+        # 定义 Critic 网络的损失和训练操作，并结合目标网络的软更新
+        # 这行代码确保 target_update 操作在当前定义的操作之前执行。
+        # target_update 是一个列表，包含将参数更新到目标网络的操作。
+        # 通过软更新（Exponential Moving Average）将当前网络的参数缓慢地替换到目标网络中，以提高训练的稳定性。
+        with tf.control_dependencies(target_update):  # soft replacement happened at here
             q_target = self.R + GAMMA * q_
+            # D误差用于衡量当前估计的Q值与实际Q值（通过Bellman方程计算得到）之间的差距
+            # 通过最小化TD误差，可以更新Critic网络，使其对状态-动作对的Q值估计更加准确。
             td_error = tf.losses.mean_squared_error(labels=q_target, predictions=q)
             self.ctrain = tf.train.AdamOptimizer(LR_C).minimize(td_error, var_list=c_params)
 
@@ -82,8 +89,9 @@ class DDPG(object):
         ba = bt[:, self.s_dim: self.s_dim + self.a_dim]
         br = bt[:, -self.s_dim - 1: -self.s_dim]
         bs_ = bt[:, -self.s_dim:]
-
+        # 训练动作选择神经网络
         self.sess.run(self.atrain, {self.S: bs})
+        # 训练评估神经网络
         self.sess.run(self.ctrain, {self.S: bs, self.a: ba, self.R: br, self.S_: bs_})
 
     def store_transition(self, s, a, r, s_):
@@ -133,18 +141,18 @@ for i in range(MAX_EPISODES):
 
         # Add exploration noise
         a = ddpg.choose_action(s)
-        a = np.clip(np.random.normal(a, var), -2, 2)    # add randomness to action selection for exploration
+        a = np.clip(np.random.normal(a, var), -2, 2)  # add randomness to action selection for exploration
         s_, r, done, info = env.step(a)
 
         ddpg.store_transition(s, a, r / 10, s_)
 
         if ddpg.pointer > MEMORY_CAPACITY:
-            var *= .9995    # decay the action randomness
+            var *= .9995  # decay the action randomness
             ddpg.learn()
 
         s = s_
         ep_reward += r
-        if j == MAX_EP_STEPS-1:
+        if j == MAX_EP_STEPS - 1:
             print('Episode:', i, ' Reward: %i' % int(ep_reward), 'Explore: %.2f' % var, )
             # if ep_reward > -300:RENDER = True
             break
